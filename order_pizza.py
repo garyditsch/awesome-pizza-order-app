5bdf1f07a27fa08f744a95c29b6cd3136fd03ec5
Gary Ditsch <garyditsch@gmail.com>
2016-08-29 11:08:00 -0400

    order gets added to list

--- order_pizza.py
+++ order_pizza.py
@@ -1,9 +1,48 @@
+PIZZAS = (
+    {
+        "name": "Cheese Pizza",
+        "cost": 5.00
+    },
+    {
+        "name": "Pepperoni Pizza", 
+        "cost": 7.50
+    },
+)
+
+my_pizzas = []
+
 def display_invalid_option(menu_selection):
 	if menu_selection.isdigit():
 		print("\n{} is an invalid option, please try again" .format(menu_selection))
 	else:
 	    print("\n{} is not a number, please enter a number from the menu above" .format(menu_selection))
 
+def is_valid_pizza(pizza_selection, pizzas):
+	if pizza_selection.isdigit() and int(pizza_selection)-1 < len(pizzas):
+		return True
+	else:
+		return False
+
+def add_to_order():
+	"""
+	Prompts for adding pizza to the order
+	"""
+
+	while True: 
+		for index, pizza in enumerate(PIZZAS):
+			print("{}: {}".format(index+1, pizza["name"]))
+		print("0: Go back")
+
+		pizza_selection = input("\nWhich pizza would you like to order?")
+
+		if pizza_selection == "0":
+			break
+		elif is_valid_pizza(pizza_selection, PIZZAS):
+			my_pizzas.append(PIZZAS[int(pizza_selection) -1])
+		else: 
+			pass
+
+		print(my_pizzas)
 
 
 def main():
@@ -24,6 +63,7 @@ def main():
         if menu_selection == "0":
         	break
         elif menu_selection == "1":
+        	add_to_order()
         	pass
         elif menu_selection == "2":
         	pass
@@ -35,4 +75,5 @@ def main():
         	display_invalid_option(menu_selection)
 
 
-main()
+if __name__ == '__main__':
+    main()
