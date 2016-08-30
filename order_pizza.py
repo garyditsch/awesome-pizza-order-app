cb27695f9042ae5cfc8c9f1b8cbbd81f20294da8
Gary Ditsch <garyditsch@gmail.com>
2016-08-30 10:17:41 -0400

    updating remove feature

--- order_pizza.py
+++ order_pizza.py
@@ -12,40 +12,85 @@ PIZZAS = (
 my_pizzas = []
 
 def display_invalid_option(menu_selection):
-	if menu_selection.isdigit():
-		print("\n{} is an invalid option, please try again" .format(menu_selection))
-	else:
-	    print("\n{} is not a number, please enter a number from the menu above" .format(menu_selection))
+    if menu_selection.isdigit():
+        print("\n{} is an invalid option, please try again" .format(menu_selection))
+    else:
+        print("\n{} is not a number, please enter a number from the menu above" .format(menu_selection))
 
 def is_valid_pizza(pizza_selection, pizzas):
-	if pizza_selection.isdigit() and int(pizza_selection)-1 < len(pizzas):
-		return True
-	else:
-		return False
+    if pizza_selection.isdigit() and int(pizza_selection)-1 < len(pizzas):
+        return True
+    else:
+        return False
+
+def display_pizzas(pizzas):
+    if len(pizzas) > 0:
+        for index, pizza in enumerate(pizzas):
+            print("{}: {} cost: ${:,.2f}".format(index+1, pizza["name"], pizza["cost"]))
+    else:
+        print("No pizzas found.")
+
+def display_total_cost(pizzas):
+    total_cost = sum(pizza["cost"] for pizza in pizzas)
+    print("======================")
+    print("Total Cost: {}".format(total_cost))
+
+def display_order(pizzas):
+    display_pizzas(pizzas)
+    display_total_cost(pizzas)
+    print("\n\n")
+
+
 
 def add_to_order():
-	"""
-	Prompts for adding pizza to the order
-	"""
+    """
+    Prompts for adding pizza to the order
+    """
 
-	while True: 
-		for index, pizza in enumerate(PIZZAS):
-			print("{}: {}".format(index+1, pizza["name"]))
-		print("0: Go back")
+    while True: 
+        display_pizzas(PIZZAS)
+        print("0: Go back")
 
-		pizza_selection = input("\nWhich pizza would you like to order?")
+        pizza_selection = input("\nWhich pizza would you like to order?")
 
-		if pizza_selection == "0":
-			break
-		elif is_valid_pizza(pizza_selection, PIZZAS):
-			my_pizzas.append(PIZZAS[int(pizza_selection) -1])
-		else: 
-			pass
+        if pizza_selection == "0":
+            break
+        elif is_valid_pizza(pizza_selection, PIZZAS):
+            my_pizzas.append(PIZZAS[int(pizza_selection) -1])
+        else: 
+            pass
 
-		print(my_pizzas)
+def remove_from_order():
+    """
+    Remove a pizza from my_pizzas based on user's input
+    """
+    global my_pizzas
+
+    while True: 
+        display_order(my_pizzas)
+        print("0: Go back")
+
+        pizza_selection = input("\nWhich pizza would you like to remove?")
+
+        if pizza_selection == "0":
+            break
+        elif is_valid_pizza(pizza_selection, my_pizzas):
+            del my_pizzas[int(pizza_selection) -1]
+        else: 
+            display_invalid_option(pizza_selection)
+
+def order_pizza(pizzas):
+    if len(pizzas) > 0:
+        print("Thank You for your order")
+        return True
+    else:
+        print("Please add pizzas to your order before completing the order")
+        return False 
 
 
 def main():
+    global my_pizzas
+
     MENU_ITEMS = (
        "1: Add Pizza to Order", 
        "2: Remove Pizza from Order",
@@ -62,18 +107,22 @@ def main():
         menu_selection = input("\nPlease select an option from above? ")
 
         if menu_selection == "0":
-        	break
+            break
         elif menu_selection == "1":
-        	add_to_order()
-        	pass
+            add_to_order()
+            pass
         elif menu_selection == "2":
-        	pass
+            remove_from_order()
         elif menu_selection == "3":
-        	pass
+            display_pizzas(my_pizzas)
+            print("\n\n")
         elif menu_selection == "4":
-        	pass
+            display_order(my_pizzas)
+            if order_pizza(my_pizzas):
+                my_pizzas = []
+            pass
         else:
-        	display_invalid_option(menu_selection)
+            display_invalid_option(menu_selection)
 
 
 if __name__ == '__main__':
