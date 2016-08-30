1950aa01106c28ed30a6c9d743d54557c21cefde
Gary Ditsch <garyditsch@gmail.com>
2016-08-30 14:18:28 -0400

    adding remove pizza feature

--- custom_order.py
+++ custom_order.py
@@ -8,7 +8,7 @@ def get_menu_selection(menu_items):
 
     return input("\nPlease select an option from above.")
 
-def get_menu_selection_error(menu_items):
+def display_selection_error(menu_selection):
     if menu_selection.isdigit():
         print("\n{} is an invalid option, please try again" .format(menu_selection))
     else:
@@ -22,6 +22,9 @@ class Topping():
     def __init__(self, name, price=1.00):
         self.name = name 
         self.price = price 
+
+    def __str__(self):
+        return "{} ${:,.2f}".format(self.name, self.price)
 
 
 class Pizza():
@@ -39,10 +42,9 @@ class Pizza():
         Topping(name="Sausage", price=2.50)
     )
 
-
-
-    def __init__(self):
-        pass
+    def __init__(self, base_price=5.00):
+        self.toppings = []
+        self.base_price = base_price
 
     def __init__(self):
         self.toppings = []
@@ -62,7 +64,9 @@ class Pizza():
             if menu_selection == "0":
                 return None
             elif menu_selection == "1":
-                self.add_pizza()
+                pizza.add_toppings()
+            elif menu_selection == "2":
+                pizza.display_toppings()
             elif menu_selection == "4":
                 return pizza 
             else:
@@ -70,14 +74,21 @@ class Pizza():
 
         return None
 
+    def get_total_price(self):
+        return self.base_price + sum(topping.price for topping in self.toppings)
+
     def get_toppings_menu_list(self, toppings):
         menu_items = [
-            "{}: {}".format(idex + 1, toppings)
-            for index, toppings in enumerate(toppings)
+            "{}: {}".format(index + 1, topping)
+            for index, topping in enumerate(toppings)
         ]
         menu_items.append("0: Exit")
 
         return menu_items 
+
+    def is_valid_topping(self, selection, toppings=AVAILABLE_TOPPINGS):
+        return (selection.isdigit() and int(selection) -1 < len(toppings))
+
 
     def add_toppings(self):
         while True:
@@ -86,6 +97,35 @@ class Pizza():
 
             if menu_selection == "0":
                 break
+            elif self.is_valid_topping(menu_selection):
+                topping = self.AVAILABLE_TOPPINGS[int(menu_selection) - 1]
+                self.toppings.append(topping)
+                print("\n{} added to the pizza!".format(topping))
+            else:
+                display_selection_error(menu_selection)
+
+    def display_toppings(self):
+        if len(self.toppings) == 0:
+            print("No toppings")
+        else:
+            for topping in self.toppings:
+                print(topping)
+
+        print("="*10)
+        print("Total Price: ${:,.2f}".format(self.get_total_price()))
+
+    def remove_toppings(self):
+        while True:
+            menu_selection = get_menu_selection(self.get_toppings_menu_list(self.toppings))
+
+            if menu_selection == "0":
+                break
+            elif self.is_valid_topping(menu_selection, self.toppings):
+                topping = self.toppings[int(menu_selection) -1 ]
+                self.toppings.remove(topping)
+                print("\n{} removed from the pizza.".format(topping))
+            else:
+                display_selection_error(menu_selection)
 
 class Cart():
     MENU_ITEMS = (
@@ -99,11 +139,45 @@ class Cart():
     def __init__(self):
         self.pizza = []
 
+    def get_total_price(self):
+        return sum(pizza.get_total_price() for pizza in self.pizzas)
+
+
     def add_pizza(self):
         pizza = Pizza.make_pizza()
         if pizza is not None:
             self.pizzas.append(pizza)
             print("\n Pizza added to cart!")
+
+    def display_pizzas(self):
+        if len(self.pizzas) == 0:
+            print("There are no pizzas in cart")
+        else:
+            for index, pizza in enumerate (self.pizzas):
+                print("\n{index}: Pizza {index:<10} ${price:,.2f}".format(index=index+1, pizza=price.get_total_price()))
+            pizza.display_toppings()        
+
+        print("")
+        print("*"*40)
+        print("Shopping Cart Total: ${:,.2f}".format(self.get_total_price()))
+
+    def remove_pizzas(self):
+        while True:
+            self.display_pizzas()
+            print("0: Cancel")
+            menu_selection = input("\nPlease select a pizza to remvoe. ")
+
+            if menu_selection == "0":
+                break
+            elif (menu_selection.isdigit()
+                and int(menu_selection) -1 < len(self.pizzas)):
+
+                pizza = self.pizzas[int(menu_selection) -1]
+                self.pizzas.remove(pizza)
+                print("\n{} removed from the cart.".format(menu_selection))
+            else:
+                display_selection_error(menu_selection)
+
 
     def display_menu(self):
         while True:
@@ -111,8 +185,16 @@ class Cart():
 
             if menu_selection == "0": 
                 break
-            if menu_selection == "1":
+            elif menu_selection == "1":
                 self.add_pizza()
+            elif menu_selection == "2":
+                self.display_pizzas()
+            elif menu_selection == "3":
+                self.remove_pizzas()
+            elif menu_selection == "4":
+                self.pizzas = []
+            else:
+                display_selection_error(menu_selection)
 
 
 def main():
